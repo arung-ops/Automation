f8fcdf402f92b34a8e01784ef880dcd10d015307
Arun <arun.g@nyavx.com>
2026-08-27 19:41:04 +0530

    new

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,41 +1,45 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  // Test location
   testDir: './e2e',
 
+  // Run independent tests in parallel
   fullyParallel: true,
 
+  // Prevent accidental test.only in CI
   forbidOnly: !!process.env.CI,
 
+  // Retry failed tests only in CI
   retries: process.env.CI ? 2 : 0,
 
+  // Parallel workers
+  // Local machine  -> Playwright decides
+  // GitHub Actions  -> 4 workers
   workers: process.env.CI ? 4 : undefined,
 
+  // Test timeout
   timeout: 60_000,
 
+  // Assertion timeout
   expect: {
     timeout: 10_000,
   },
 
+  // Reports
   reporter: [
     ['list'],
 
     [
       'html',
       {
-        outputFolder: './reports/playwright-report',
+        outputFolder: './reports/',
         open: 'never',
-      },
-    ],
-
-    [
-      'allure-playwright',
-      {
-        outputFolder: 'reports/allure-results',
       },
     ],
   ],
 
+  // Common browser settings
   use: {
     baseURL: process.env.BASE_URL || 'https://demoqa.com',
 
@@ -52,6 +56,7 @@ export default defineConfig({
     navigationTimeout: 30_000,
   },
 
+  // Browser
   projects: [
     {
       name: 'chromium',
